5ba7ec79c45acc3ee59c312a01d81b5793ecf6f6
SRC123123 <SRCummings.1993@gmail.com>
2016-09-18 19:39:41 -0400

    Homework 2 Upload

---
+++ COSC-311-HW2/src/Main.java
@@ -0,0 +1,43 @@
+//Shawn Cummings
+//https://github.com/SRC123123/Java/tree/master/COSC-311-HW2
+//COSC 311
+//HW 09/14
+//Fall 2016
+
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int [] deck = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		boolean [] index = new boolean [deck.length];
+		int curr = 0;
+		Random gen = new Random();		
+		gen.setSeed(System.nanoTime());
+		
+		
+		int x =0;
+		while (x < 3)
+		{	
+			while(true){
+			curr = gen.nextInt(9);
+
+				if(index[curr]==false)
+				{
+					index[curr] = true;
+					break;					
+				}			
+			}
+			x++;
+			curr = gen.nextInt(9);			
+		}
+		
+		for(int i = 0; i < deck.length; i++)
+		{
+			if(index[i]== true)
+			{
+				System.out.println(deck[i]);
+			}
+		}
+	}
+}
