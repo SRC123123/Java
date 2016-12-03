58eb95af55de52296ff1e4758b61858f2a10fe98
SRC123123 <SRCummings.1993@gmail.com>
2016-12-03 18:17:35 -0500

    COSC-311-PP2 - Initial Upload
    The first upload to GH of my Programming Project 2 for my COSC 311
    course.

---
+++ COSC-311-PP2/src/Main.java
@@ -0,0 +1,93 @@
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//Initialize		
+		Random gen = new Random();
+		int [] nRanges = {4, 8, 12, 16, 20, 2000, 4000, 6000, 8000, 10000};
+		int [] cc = {5, 3, 5, 3, 1, 2, 3, 4, 6, 3, 7, 1, 2};
+
+		//Main Loop
+		//Changes the value of N
+		//Allows the program to run every variation of N we need it to
+		//so all of the Project data will be in the output
+		for(int n : nRanges){
+
+			System.out.println("N == " + n);
+
+			for(int i = 0; i < 1000; i++)
+				System.out.print("=");
+			System.out.println();
+
+			//Initialize arrays
+			int [] sc1 = new int [n];
+			int [] sc2 = new int [n];
+			int [] sc3 = new int [n];
+			int [] data = new int[n];
+			int [] temp = null;
+
+			//Populate arrays with data
+			for(int i = 0; i < n; i++)
+			{
+				sc1[i] = 1;
+				sc2[i] = i+1;
+				sc3[i] = n - i;
+				data[i] = gen.nextInt(); //Randomly generate data
+			}
+
+			//Insert/Delete/Output
+			//This will select a data set to be used
+			//Then insert all of the data into the priority queue
+			//Then delete all of the data in the priority queue
+			//Then output the results of the max age
+			for(int x = 0; x < 5; x++)
+			{	
+				//Select Data Set
+				switch(x){
+				case 0:
+					temp = cc;
+					System.out.println("Correctness Check");
+					break;
+				case 1:
+					temp = sc1;
+					System.out.println("Sanity Check 1");
+					break;
+				case 2:
+					temp = sc2;
+					System.out.println("Sanity Check 2");
+					break;
+				case 3:
+					temp = sc3;
+					System.out.println("Sanity Check 3");
+					break;
+				case 4:
+					temp = data;
+					System.out.println("Random Data");
+					break;			
+				}
+
+				//Initialize PriorityQueue - necessary to reset counter after change of data set
+				PriorityQueue cue = new PriorityQueue();
+
+				//Insert Data into Priority Queue
+				for(int i : temp)
+				{
+					cue.insert(i);
+				}			
+				cue.print(); //Print list of all data in queue
+				System.out.println();
+
+				//Delete Data inside the Priority Queue
+				for(int i : temp)
+				{
+					cue.delete();			
+				}
+				cue.print(); //Print max
+				System.out.println();
+			}
+		}
+
+	}
+}
---
+++ COSC-311-PP2/src/PriorityQueue.java
@@ -0,0 +1,112 @@
+public class PriorityQueue {
+	private Node head;
+	private int counter;
+	private int [] max;
+
+	//Constructor
+	public PriorityQueue()
+	{
+		counter = 0;
+		max = new int [2];
+	}
+
+	//Inserting Node into Queue
+	public void insert(int data)
+	{	
+		Node node = new Node(data, counter);
+		sortNode(node, head);
+		counter++;
+	}
+
+	//Finding the Node's proper location and insert it there
+	private void sortNode(Node node, Node head)
+	{	
+		if(head == null) //If head is null, node is first to enter queue
+		{
+			this.head = node;
+		}		
+		else if(head.data > node.data) //Node is smaller than head, first in queue
+		{	
+			if(head.prev != null)
+				head.prev.next = node;
+
+			node.prev = head.prev;
+			head.prev = node;
+			node.next = head;
+
+			if(node.prev == null) //Check Head
+				this.head = node;
+		}
+		else if(head.next == null) //End of Queue, insert node
+		{
+			head.next = node;
+			node.prev = head;
+		}
+		else //node is larger than head, continue search
+		{
+			sortNode(node, head.next);
+		}				
+	}
+
+	//Delete Head
+	public void delete()
+	{
+		if(head == null)
+			return;	
+
+		max();
+
+		if(this.head.next != null)
+		{
+			this.head = this.head.next;
+			this.head.prev.next = null;
+			this.head.prev = null;
+		}
+		else
+		{
+			head = null;
+		}
+		counter++;
+	}
+
+	//Check current max
+	private void max()
+	{
+		if(counter - this.head.ts > max[1])
+		{
+			max[0] = this.head.data; //max[0] == data
+			max[1] = counter - this.head.ts; //max[1] == age of data
+		}		
+	}
+
+	//Print Queue
+	public void print()
+	{
+		//Print Max
+		if(head == null)
+			System.out.print("The oldest element is data = " + max[0] + ", with age = " + max[1] + "\n");
+		
+		//Print Everything Else
+		Node temp = head;
+
+		while(temp != null)
+		{
+			System.out.print("(D: " + temp.data + " TS: " + temp.ts + "), ");
+			temp = temp.next;
+		}	
+	}
+
+	//Private Node Class
+	private class Node {
+		private int data;
+		private int ts;
+		private Node next;
+		private Node prev;
+
+		public Node(int data, int ts)
+		{
+			this.data = data;
+			this.ts = ts;
+		}
+	}
+}
